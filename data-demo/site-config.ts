21078effc700e7c5005c37613505399872bb0b78
Paul <paul@theapplegates.com>
2025-07-18 10:25:14 -0400

    new/old

--- data-demo/site-config.ts
+++ data-demo/site-config.ts
@@ -2,61 +2,56 @@ import { SiteConfig } from '@/lib/site-config'
 import { DeepPartial } from '@/lib/utils'
 
 const userConfig: DeepPartial<SiteConfig> = {
-  siteUrl: 'https://analog-demo.zhutmost.com',
-  siteTitle: 'Analog Demo',
+  siteUrl: 'https://analog.paulapplegate.com',
+  siteTitle: "Live Life",
   description:
-    'Explore Analog: An open-source blog template built with Next.js by zhutmost. Perfect for technical contents, research teams, and academic blogs. Modern, SEO-optimized, and fully customizable.',
+    "This is my piece on the internet. I write about my thoughts, and life.",
   locale: 'en-US',
-  author: 'John Doe',
+  author: 'Paul Applegate',
   homepage: {
-    sections: ['latestNews', 'popularTags', 'recentPosts'],
-    popularTags: [
-      { tag: 'markdown', icon: 'IconMarkdown', title: 'Markdown' },
-      { tag: 'mac', icon: 'IconCommand', title: 'MacOS' },
-      { tag: 'shopping', icon: 'IconTag', title: 'Shopping' },
-      { tag: 'writing', icon: 'IconNote', title: 'Writing' },
-      { tag: 'windows', icon: 'IconBrandWindows', title: 'Windows' },
-    ],
-    githubCalendar: 'zhutmost',
+    sections: ['latestNews', 'recentPosts'],
+    popularTags: [],
+    githubCalendar: '',
   },
   header: {
     logo: '/icon.svg',
-    title: 'Analog Demo',
+    title: "Paul's Blog",
     themeSwitch: true,
     menu: {
       Home: '/',
       Blog: '/archive',
-      News: '/news',
       Tags: '/tags',
-      Team: '/team',
+      News: '/news',
+      Publication: '/publication',
       About: '/about',
     },
   },
   footer: {
-    // beian: '粤ICP备2021******号',
     icons: {
-      Facebook: { icon: 'IconBrandFacebook', href: 'https://facebook.com' },
-      X: { icon: 'IconBrandX', href: 'https://x.com' },
+      GitHub: { icon: 'IconBrandGithub', href: 'https://github.com/theapplegates' },
       RSS: { icon: 'IconRss', href: '/rss.xml' },
       Analytics: {
         icon: 'IconChartDots',
-        href: 'https://cloud.umami.is/share/unndQ4BKaNaSgfys/analog-demo.zhutmost.com',
+        href: '',
       },
     },
   },
   analytics: {
     umamiAnalytics: {
-      umamiWebsiteId: '4ae56858-7872-446f-980e-32d1c8db927e',
+      umamiWebsiteId: '',
     },
   },
   comment: {
-    provider: 'giscus',
+    provider: '',
     giscusConfig: {
-      repo: 'zhutmost/analog-blog-starter',
-      repoId: 'R_kgDONEFqpw',
+      repo: 'zhutmost/zhutmost-blog',
+      repoId: 'R_kgDONUD9-A',
       category: 'Announcements',
-      categoryId: 'DIC_kwDONEFqp84CjmEV',
+      categoryId: 'DIC_kwDONUD9-M4Ckkm1',
     },
+  },
+  seo: {
+    socialBanner: '/banner-zhutmost-com.png',
   },
   license: 'cc-by-nc-sa',
 }
