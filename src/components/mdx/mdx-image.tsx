ad74a8804eb2cd198080d47e29b45107c391792f
Paul <paul@theapplegates.com>
2025-10-03 13:39:27 -0400

    Update mdx-image.tsx

--- src/components/mdx/mdx-image.tsx
+++ src/components/mdx/mdx-image.tsx
@@ -14,6 +14,27 @@ export default function MdxImage({
   if (!src) return null
 
   const imgTitle = title ?? alt ?? ''
+  <picture>
+<img
+sizes="(max-width: 1400px) 100vw, 1400px"
+srcset="
+kjehl8vrltp80kuayyrq_c_scale,w_200.jpg 200w,
+kjehl8vrltp80kuayyrq_c_scale,w_310.jpg 310w,
+kjehl8vrltp80kuayyrq_c_scale,w_418.jpg 418w,
+kjehl8vrltp80kuayyrq_c_scale,w_512.jpg 512w,
+kjehl8vrltp80kuayyrq_c_scale,w_588.jpg 588w,
+kjehl8vrltp80kuayyrq_c_scale,w_714.jpg 714w,
+kjehl8vrltp80kuayyrq_c_scale,w_815.jpg 815w,
+kjehl8vrltp80kuayyrq_c_scale,w_911.jpg 911w,
+kjehl8vrltp80kuayyrq_c_scale,w_974.jpg 974w,
+kjehl8vrltp80kuayyrq_c_scale,w_1086.jpg 1086w,
+kjehl8vrltp80kuayyrq_c_scale,w_1171.jpg 1171w,
+kjehl8vrltp80kuayyrq_c_scale,w_1254.jpg 1254w,
+kjehl8vrltp80kuayyrq_c_scale,w_1329.jpg 1329w,
+kjehl8vrltp80kuayyrq_c_scale,w_1400.jpg 1400w"
+src="kjehl8vrltp80kuayyrq_c_scale,w_1400.jpg"
+alt="">
+</picture>
 
   return (
     <VStack as="figure" my="1.625em">
